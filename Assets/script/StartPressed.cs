ec1ac7637dc78d3ed5bfeb8d000b3b7ca488218b
Rock Gu <guf@mcmaster.ca>
2018-01-28 02:27:20 -0500

    UI Flow Finished

---
+++ Assets/script/StartPressed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPressed : MonoBehaviour {
+	public GameObject score = GameObject.Find("score");
+	private UIDisplayFlow UIScript;
+	// Use this for initialization
+	void Start () {
+		UIScript = score.GetComponent<UIDisplayFlow>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onTriggerEnter (Collider other){
+		this.gameObject.setActive(false);
+		UIScript.inGame = true;
+
+	}
+}
---
+++ Assets/script/UIDisplayFlow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplayFlow : MonoBehaviour {
+	public const int PRE_GAME = 0;
+	public const int IN_GAME = 1;
+	public const int END_GAME_WON = 2;
+	public const int END_GAME_LOST = 3;
+	public const int GAME_WAIT = 4;
+
+	public int state;
+
+	public bool result;
+	public bool inGame;
+
+	public Text text;
+	public Text scoreText;
+	public int score;
+	// Use this for initialization
+	void Start () {
+		state = GAME_WAIT;
+		result = false;
+		inGame = false;
+		text.text = "";
+		score = 0;
+		scoreText.text = "Score: "+ score;
+	}
+	void updateScore(){
+
+		scoreText.text = "Score: "+ score;
+	}	
+	// Update is called once per frame
+	void Update () {
+		switch(state){
+			case PRE_GAME:
+				//clean all the stuff u drew
+				//inGame needs to be changed when triggered.
+				updateScore();
+				if(inGame){
+					state = IN_GAME;
+				}
+				break;
+			case IN_GAME:
+				if(!inGame){
+					if(result){
+						state = END_GAME_WON;
+						text.text = "Congratulations!"
+						score += 1;
+						updateScore();
+					} else {
+						state = END_GAME_LOST;
+						text.text = "Feelsbadman."
+					}
+
+				}
+				break;
+			case END_GAME_WON:
+				state = GAME_WAIT;
+				break;
+			case END_GAME_LOST:
+				score = 0;
+				state = GAME_WAIT;
+				break;
+			case GAME_WAIT:
+				break;
+		}
+	}
+}
